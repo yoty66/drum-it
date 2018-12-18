60ff293d272af8fee48b66d03ec06a7ab59b3da9
SaritSass <sarit@sassfamily.net>
2018-12-18 18:21:00 +0200

    css+latest

--- src/Panels/EnablePanel.js
+++ src/Panels/EnablePanel.js
@@ -1,30 +1,30 @@
 import Drum from '../Components/Drum.js';
-import React,{Component} from 'react';
-import {Col} from 'react-bootstrap'
-import {connect} from 'react-redux'
-import {renderPanel} from './renderPanelFunctions'
-import DrumEnabler from "../Components/DrumEnabler";
+    import React,{Component} from 'react';
+    import {Col} from 'react-bootstrap'
+    import {connect} from 'react-redux'
+    import {renderPanel} from './renderPanelFunctions'
+    import DrumEnabler from "../Components/DrumEnabler";
 
 
 
-class EnablePanel extends Component {
-    constructor(props) {
-        super(props)
-        this.renderCol=this.renderCol.bind(this);
+    class EnablePanel extends Component {
+        constructor(props) {
+            super(props)
+            this.renderCol=this.renderCol.bind(this);
+        }
+        render(){
+
+            return(renderPanel(this.renderCol));
+        }
+        renderCol=(id)=> {
+            return(
+                <Col md={3} lg={3} lgHidden={true} mdHidden={true}>
+                    <DrumEnabler id={id} />
+                </Col>);
+        };
     }
-    render(){
 
-        return(renderPanel(this.renderCol));
-    }
-    renderCol=(id)=> {
-        return(
-            <Col md={3} lg={3} lgHidden={true} mdHidden={true}>
-                <DrumEnabler id={id} />
-            </Col>);
-    };
-}
-
-const mapStateToProps=(state)=> (
-    { instruments : state.instruments , order : state.order}
-);
-export default connect(mapStateToProps)(EnablePanel);
+    const mapStateToProps=(state)=> (
+        { instruments : state.instruments , order : state.order}
+    );
+    export default connect(mapStateToProps)(EnablePanel);
