a3887b3971f28c22ab3ce96e43209e745752597a
Avraham <Yotam.Avraham@carlsonwagonlit.com>
2019-02-11 09:26:26 +0200

    Lesson is working (the constant)

--- src/Panels/ControlPanel.js
+++ src/Panels/ControlPanel.js
@@ -17,10 +17,40 @@ class ControlPanel extends Component {
 
 
     render = () => {
-        const SessionExample=[{id:1,timeStamp:2000},{id:2,timeStamp:2000},{id:3,timeStamp:3000},{id:4,timeStamp:4000}];
-        const tempotest=9;
-        const SessionExample2=SessionExample.map(({id,timeStamp})=>( {id:id, timeStamp:(timeStamp*tempotest) }) )
-        console.log(SessionExample2);
+        const SessionExampleBass= [{id:7,timeStamp:2000},{id:7,timeStamp:18000},{id:7,timeStamp:22000}];
+        const SessionExampleSnare= [{id:6,timeStamp:10000},{id:6,timeStamp:26000}];
+        const SessionExampleHH= [{id:5,timeStamp:14000},{id:5,timeStamp:16000},{id:5,timeStamp:30000}];
+        const SessionExampleBigTom= [{id:3,timeStamp:4000}];
+        const SessionExampleSmallTom=[{id:2,timeStamp:6000}];
+
+        const SessionExamplePart1 = SessionExampleBass.concat(SessionExampleSnare);
+        const SessionExamplePart2 = SessionExamplePart1.concat(SessionExampleHH);
+        const SessionExamplePart3 = SessionExamplePart2.concat(SessionExampleBigTom);
+        const SessionExamplePart4 = SessionExamplePart3.concat(SessionExampleSmallTom);
+
+        const offset = 32000;
+
+        const SessionExPart1=SessionExamplePart1.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + offset)}));
+        const SessionPart1 = SessionExampleBass.concat(SessionExPart1);
+        console.log(SessionPart1);
+
+        const SessionExPart2 = SessionExamplePart2.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (2 * offset))}));
+        const SessionPart2 = SessionPart1.concat(SessionExPart2);
+        console.log(SessionPart2);
+
+        const SessionExPart3 = SessionExamplePart3.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (3 * offset))}));
+        const SessionPart3 = SessionPart2.concat(SessionExPart3);
+        console.log(SessionPart3);
+
+        const SessionExPart4 = SessionExamplePart4.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (4 * offset))}));
+        const SessionPart4 = SessionPart3.concat(SessionExPart4);
+        console.log(SessionPart4);
+
+        //here we adjust the tempo (if necessary)
+        const tempotest=5;
+        const SessionExample = SessionPart4.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp/tempotest)}));
+
+        console.log(SessionExample);
 return(
 <div>
 
--- src/reducers/reducer-glowingDrumController.js
+++ src/reducers/reducer-glowingDrumController.js
@@ -1,4 +1,4 @@
-const initialState=[undefined, false, false, false, false, false, false, false,true];
+const initialState=[undefined, false, false, false, false, false, false, false,false];
 export default (state=initialState,action)=>
 {
     if(action.type==='ENABLE_GLOWING')
