137fa16d9d0f3755371ec5443ab618cfa4171ef3
SaritSass <sarit@sassfamily.net>
2019-02-16 14:34:59 +0200

    update lesson 1

--- src/Panels/ControlPanel.js
+++ src/Panels/ControlPanel.js
@@ -13,50 +13,71 @@ class ControlPanel extends Component {
         // this.StartSession=this.StartSession.bind(this);
         // this.EndSession=this.EndSession.bind(this);
         this.SessionControler = SessionControler.bind(this);
+
     }
+         render = () => {
+             const SessionExampleBass= [{id:7,timeStamp:2000},{id:7,timeStamp:18000},{id:7,timeStamp:22000}];
+             const SessionExampleSnare= [{id:6,timeStamp:10000},{id:6,timeStamp:26000}];
+             const SessionExampleHH= [{id:5,timeStamp:14000},{id:5,timeStamp:16000},{id:5,timeStamp:30000}];
+             const SessionExampleBigTom= [{id:3,timeStamp:4000}];
+           //  const SessionExampleSmallTom=[{id:2,timeStamp:6000}];
+
+             const SessionExamplePart1 = SessionExampleBass.concat(SessionExampleSnare);
+             const SessionExamplePart2 = SessionExamplePart1.concat(SessionExampleHH);
+             const SessionExamplePart3 = SessionExamplePart2.concat(SessionExampleBigTom);
+           //  const SessionExamplePart4 = SessionExamplePart3.concat(SessionExampleSmallTom);
+
+             const offset = 32000;
+
+             const SessionBass = SessionExampleBass.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + offset)}));
+             const SessionAddBass = SessionExampleBass.concat(SessionBass)
+             console.log(SessionAddBass);
+
+             const SessionSnare1 = SessionExamplePart1.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (2 * offset))}));
+             const SessionAddSnare1 = SessionAddBass.concat(SessionSnare1);
+             console.log(SessionAddSnare1);
+
+             const SessionSnare2 = SessionExamplePart1.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (3 * offset))}));
+             const SessionAddSnare2 = SessionAddSnare1.concat(SessionSnare2);
+             console.log(SessionAddSnare2);
+
+             const SessionHH1 = SessionExamplePart2.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (4 * offset))}));
+             const SessionAddHH1 = SessionAddSnare2.concat(SessionHH1);
+             console.log(SessionAddHH1);
+
+             const SessionHH2 = SessionExamplePart2.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (5 * offset))}));
+             const SessionAddHH2 = SessionAddHH1.concat(SessionHH2);
+             console.log(SessionAddHH2);
+
+             const SessionBigTom1 = SessionExamplePart3.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (6 * offset))}));
+             const SessionAddBigTom1 = SessionAddHH2.concat(SessionBigTom1);
+             console.log(SessionAddBigTom1);
+
+             const SessionBigTom2 = SessionExamplePart3.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (7 * offset))}));
+             const SessionAddBigTom2 = SessionAddBigTom1.concat(SessionBigTom2);
+             console.log(SessionAddBigTom2);
+
+    //         const SessionExPart4 = SessionExamplePart4.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (4 * offset))}));
+    //         const SessionPart4 = SessionPart3.concat(SessionExPart4);
+    //         console.log(SessionPart4);
+
+             //here we adjust the tempo (if necessary)
+             const tempotest=3;
+             const SessionExample = SessionAddBigTom2.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp/tempotest)}));
+
+             console.log(SessionExample);
 
 
-    render = () => {
-        const SessionExampleBass= [{id:7,timeStamp:2000},{id:7,timeStamp:18000},{id:7,timeStamp:22000}];
-        const SessionExampleSnare= [{id:6,timeStamp:10000},{id:6,timeStamp:26000}];
-        const SessionExampleHH= [{id:5,timeStamp:14000},{id:5,timeStamp:16000},{id:5,timeStamp:30000}];
-        const SessionExampleBigTom= [{id:3,timeStamp:4000}];
-        const SessionExampleSmallTom=[{id:2,timeStamp:6000}];
 
-        const SessionExamplePart1 = SessionExampleBass.concat(SessionExampleSnare);
-        const SessionExamplePart2 = SessionExamplePart1.concat(SessionExampleHH);
-        const SessionExamplePart3 = SessionExamplePart2.concat(SessionExampleBigTom);
-        const SessionExamplePart4 = SessionExamplePart3.concat(SessionExampleSmallTom);
-
-        const offset = 32000;
-
-        const SessionExPart1=SessionExamplePart1.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + offset)}));
-        const SessionPart1 = SessionExampleBass.concat(SessionExPart1);
-        console.log(SessionPart1);
-
-        const SessionExPart2 = SessionExamplePart2.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (2 * offset))}));
-        const SessionPart2 = SessionPart1.concat(SessionExPart2);
-        console.log(SessionPart2);
-
-        const SessionExPart3 = SessionExamplePart3.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (3 * offset))}));
-        const SessionPart3 = SessionPart2.concat(SessionExPart3);
-        console.log(SessionPart3);
-
-        const SessionExPart4 = SessionExamplePart4.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp + (4 * offset))}));
-        const SessionPart4 = SessionPart3.concat(SessionExPart4);
-        console.log(SessionPart4);
-
-        //here we adjust the tempo (if necessary)
-        const tempotest=5;
-        const SessionExample = SessionPart4.map(({id,timeStamp})=>({id:id, timeStamp:(timeStamp/tempotest)}));
-
-        console.log(SessionExample);
 return(
 <div>
 
 <ButtonToolbar>
 <Button onClick={() => {this.SessionControler("startSession",SessionExample)} } className = "enableButton" >Start Session</Button>
 <Button onClick={()=>{this.SessionControler("endSession")}} className = "enableButton">End Session</Button>
+<Button className="enableButton" id={9}> Metronome </Button>
+
+
 {/*<Button onClick={() => {*/}
     {/*this.SessionControler(SessionExample)*/}
 {/*}} className = "enableButton">TEst glow</Button>*/}
